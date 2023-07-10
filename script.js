8a5a24425b370d1e71e2d12320d0541852b3303a
javvadqamar <58330678+javvadqamar@users.noreply.github.com>
2023-07-10 11:57:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+setInterval(() => {
+    d = new Date();
+    hourTime = d.getHours();
+    minuteTime = d.getMinutes();
+    secondTime = d.getSeconds();
+    hourRotation = 30*hourTime + minuteTime/2;
+    minuteRotation = 6*minuteTime;
+    secondRotation = 6*secondTime;
+
+    hours.style.transform = `rotate(${hourRotation}deg`;
+    minutes.style.transform = `rotate(${minuteRotation}deg`;
+    seconds.style.transform = `rotate(${secondRotation}deg`;
+    
+}, 1000)
